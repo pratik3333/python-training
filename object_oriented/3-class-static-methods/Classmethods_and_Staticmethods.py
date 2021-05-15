db59dbaf743dda99ffd14b17568909b7e5409c61
pratik kagale <pratikkagale3333@gmail.com>
2021-05-15 10:31:55 +0530

    Classmethods and Staticmethods

---
+++ object_oriented/3-class-static-methods/Classmethods_and_Staticmethods.py
@@ -0,0 +1,42 @@
+class Employee:
+    raise_amount=1.04
+    number_of_emps=0
+    def __init__(self,first,last,pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email=first+last+'123@gmail.com'
+
+        Employee.number_of_emps +=1
+
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+    @classmethod
+    def from_string(cls,emp_string):
+        first,last,pay=emp_string.split('-')
+        return cls(first,last,pay)
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amount=amount
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or  day.weekday() == 6:
+            return False
+        return True
+
+
+
+emp1=Employee('Corey','Schafer',78000)
+emp2=Employee('Pratik','Kagale',78000)
+emp3='John-Smith-98000'
+
+new_emp3=Employee.from_string(emp3)
+print(Employee.number_of_emps)
+
+import datetime
+my_date = datetime.date(2021,5,15)
+
+print(Employee.is_workday(my_date))
