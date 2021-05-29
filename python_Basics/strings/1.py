7061b6e096f4c67af4774c0502b7028901dae3de
pratik kagale <pratikkagale3333@gmail.com>
2021-05-29 10:23:33 -0700

    .format() and f'' string

---
+++ python_Basics/strings/1.py
@@ -0,0 +1,14 @@
+
+#format and f'' string
+
+greeting='Hello'
+name='pratik'
+
+#format()
+message='{}. {}, welcome!'.format(greeting,name)
+
+#f''
+message=f'{greeting}. {name}, welcome!'
+
+print(message)
+print(message)
