1407b849ce1c500e6c5b4f700b0fec0c6e75ab6f
pratik kagale <pratikkagale3333@gmail.com>
2021-05-29 14:58:12 -0700

    Filter function

---
+++ python_Basics/Filter.py
@@ -0,0 +1,11 @@
+def greater_than_2(n):
+    if n>2:
+        return True
+    else:
+        return False
+
+h1=[1,2,3,4,5,6,7,8,9,0,4]
+
+greater_th_2=list(filter(greater_than_2,h1))
+
+print(greater_th_2)
