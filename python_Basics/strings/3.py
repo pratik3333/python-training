8817314d87cac57a48d625faa05b99fb7ee166bb
pratik kagale <pratikkagale3333@gmail.com>
2021-05-29 11:39:22 -0700

    strings functions

--- python_Basics/strings/3.py
+++ python_Basics/strings/3.py
@@ -6,3 +6,9 @@ print(len(mystr))
 print(mystr[0:19:1])
 
 print(mystr[0:19:2])
+
+print(mystr.endswith('boy'))
+print(mystr.count('o'))
+print(mystr.capitalize())
+print(mystr.upper())
+print(mystr.lower())
