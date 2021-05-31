cc0a00e22030ab58ade87b29a903031bc3e02bb8
pratik kagale <pratikkagale3333@gmail.com>
2021-05-31 10:48:30 -0700

    sorting list and reverse

---
+++ python_Basics/Sorting_L_T_O/1.py
@@ -0,0 +1,15 @@
+
+
+li=[9,4,5,7,2,6,1,8,3]
+
+s_li=sorted(li)
+print('sorted variable:\t',s_li)
+
+#reverse
+s_li=sorted(li,reverse=True)
+print('reverse variable:\t',s_li)
+
+print('original variable:\t',li)
+
+li.sort(reverse=True)
+print('original variable:\t',li)
