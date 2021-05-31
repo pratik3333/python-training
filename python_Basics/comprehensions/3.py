8659307a57953b3148f196c34409b17693c35a05
pratik kagale <pratikkagale3333@gmail.com>
2021-05-30 19:20:37 -0700

    finding even numbers in list

---
+++ python_Basics/comprehensions/3.py
@@ -0,0 +1,15 @@
+
+
+nums=[1,2,3,4,5,6,7,8,9,10]
+
+#i want 'n' for each 'n' in nums if 'n' is even
+
+
+my_list=[]
+for n in nums:
+    if n % 2 ==0:
+        my_list.append(n)
+print(my_list)
+
+my_list=[n for n in nums if n%2==0]
+print(my_list)
