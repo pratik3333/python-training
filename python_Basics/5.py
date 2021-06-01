2deb4915460b9eed9c8b763088026c5c0714ae21
pratik kagale <pratikkagale3333@gmail.com>
2021-05-31 18:21:51 -0700

    os.makedirs

---
+++ python_Basics/5.py
@@ -0,0 +1,17 @@
+
+
+import os
+print(os.getcwd())
+
+#os.makedirs('demo-1/sub-dir-1')
+#
+#  os.mkdir('demo-1')
+# print(os.mkdir())
+
+#deleting the dir
+#os.removedirs('demo-1/sub-dir-1')
+
+#rename the file
+os.rename('00.py','1.py')
+
+print(os.listdir)
