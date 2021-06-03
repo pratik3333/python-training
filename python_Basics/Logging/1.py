7b888b4fdb2aa2cb52ef9c99fbeeb6ae3c4af8d4
pratik kagale <pratikkagale3333@gmail.com>
2021-06-03 10:28:03 -0700

    Logging file

---
+++ python_Basics/Logging/1.py
@@ -0,0 +1,23 @@
+
+
+import logging
+import string
+
+logging.basicConfig(filename='test.log',level=logging.DEBUG)
+
+
+
+def add(x,y):
+    return x + y
+
+
+num_1=1000
+num_2=100
+
+add_result=add(num_1,num_2)
+
+#print(f'add {num_1},{num_2} = {add_result}')
+#logging.warning(f'add {num_1},{num_2} = {add_result}')
+#logging.error(f'add {num_1},{num_2} = {add_result}')
+#logging.critical(f'add {num_1},{num_2} = {add_result}')
+logging.debug(f'add {num_1},{num_2} = {add_result}')
