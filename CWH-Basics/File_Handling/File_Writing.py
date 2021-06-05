1e5a2a88962a45262889fd4e6934c0d5cba87b12
pratik kagale <pratikkagale3333@gmail.com>
2021-06-05 13:53:17 -0700

    Open(),Read() & Readline() For Reading File

---
+++ CWH-Basics/File_Handling/File_Writing.py
@@ -0,0 +1,14 @@
+
+f=open("Robin.txt","rt")
+
+print(f.readlines())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+
+#content=f.read()
+
+#for line in f:
+ #   print(line,end="")
+#print(content)
+f.close()
