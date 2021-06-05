6712584548dd032815c05dcc9911baf52fea51e7
pratik kagale <pratikkagale3333@gmail.com>
2021-06-04 21:21:14 -0700

    Break and continue statements

---
+++ CWH-Basics/Break_and_Continue.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#
+# i=1
+#
+# while(True):
+#     print(i)
+#     if (i==111111):
+#         break
+#     i +=1
+
+while(True):
+    inp=int(input("Enter the value: "))
+    if inp>100:
+        print("Congrats you have entered a number greater than 100")
+        break
+    else:
+        print("Try again!",'\n')
+        continue
