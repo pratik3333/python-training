fdd92f19270c40fd755cb583db3b725d3b07338b
pratik kagale <pratikkagale3333@gmail.com>
2021-06-04 20:17:56 -0700

    For Loop

---
+++ CWH-Basics/2.py
@@ -0,0 +1,10 @@
+
+
+list1=[["Harry",5],
+       ["Larry",12],
+       ["Merry",55],
+       ["Carry",32]]
+dict1=dict(list1)
+
+for item,lollypop in dict1.items():
+    print(item,"and lolly is ",lollypop)
