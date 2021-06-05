1899b8ec03c5d1fc240152ea1d12f3c81d4d518b
pratik kagale <pratikkagale3333@gmail.com>
2021-06-05 13:18:03 -0700

    File IO Basics

---
+++ CWH-Basics/File_Handling/File_IO_Basics.py
@@ -0,0 +1,11 @@
+#File IO Basics
+
+"""
+"r" - Open file for reading
+"w" - Open file for writing
+"x" - creates file if not exists
+"a" - Add more content to a file
+"t" - text mode
+"b" - binary mode
+"+" - read and write
+"""
