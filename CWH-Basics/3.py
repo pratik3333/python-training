4ad06bb6d7faa31ac5d6c0b7b512b4a3f3141b42
pratik kagale <pratikkagale3333@gmail.com>
2021-06-05 11:29:38 -0700

    Doc Strings

---
+++ CWH-Basics/3.py
@@ -0,0 +1,10 @@
+
+
+
+def function2(a,b):
+    """This is a function which will calculate average of two numbers.
+    this function doesnt work for three numbers."""
+    average= (a+b)/2
+    return average
+
+print(function2.__doc__)
