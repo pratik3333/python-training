7436e6b6d0a2c6816d262bd5d7301a73349ab71c
pratik kagale <pratikkagale3333@gmail.com>
2021-06-04 18:39:38 -0700

    if else statement

--- CWH-Basics/1.py
+++ CWH-Basics/1.py
@@ -1,9 +1,17 @@
-var1=6
-var2=56
+# var1=6
+# var2=50
+#
+# var3=int(input("Enter the value: "))
+#
+# if var3>var2:
+#     print("Greater")
+# elif var3==var2:
+#     print("Equal")
+# else:
+#     print("Lesser")
 
-var3=int(input("Enter the value: "))
-
-if var3>var2:
-    print("Greater")
+list1=[3,4,6,5]
+if 5 in list1:
+    print("Yes it's in list")
 else:
-    print("Lesser")
+    print("It's not in list")
