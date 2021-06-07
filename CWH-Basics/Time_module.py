b6b07155ea460f6964a35275651d74832d00c335
pratik kagale <pratikkagale3333@gmail.com>
2021-06-07 11:30:32 -0700

    Time module

---
+++ CWH-Basics/Time_module.py
@@ -0,0 +1,16 @@
+
+
+import time
+
+initial=time.time()
+print(initial)
+
+k=0
+while (k<45):
+    time.sleep(0.5)
+    print("Pratik")
+    k +=1
+print("While loop in ran",time.time()-initial,"seconds")
+
+localtime=time.asctime(time.localtime(time.time()))
+print(localtime)
