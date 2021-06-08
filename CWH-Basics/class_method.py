eb2ea035734ab89044e98d8facda93b83e3edc78
pratik kagale <pratikkagale3333@gmail.com>
2021-06-08 15:23:16 -0700

    class method [split]

--- CWH-Basics/class_method.py
+++ CWH-Basics/class_method.py
@@ -2,6 +2,7 @@
 
 class Employee:
     no_of_leaves=5
+
     def __init__(self,first,last,salary):
         self.first=first
         self.last=last
@@ -11,9 +12,11 @@ class Employee:
     def info(self):
         print(f"The Employee name is {self.first} {self.last}.\nsalary is {self.salary}.")
 
+
     @classmethod
     def change_leaves(cls,new_leaves):
         cls.no_of_leaves=new_leaves
+
 
 emp1=Employee('pratik','kagale',9940)
 emp2=Employee('Kunal','Deshmukh',9343)
