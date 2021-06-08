ba919108d9f0da4c9fb9ebceb8edf7403cbc2843
pratik kagale <pratikkagale3333@gmail.com>
2021-06-07 19:41:48 -0700

    Join function

---
+++ CWH-Basics/join_Function.py
@@ -0,0 +1,12 @@
+
+
+
+lis=['John','cena','khali','ganesh',
+     'pratik','rupesh']
+
+#
+# for item in lis:
+#     print(item,'and',end=' ')
+# #
+a=' and '.join(lis)
+print(a,'Other wwe superstars')
