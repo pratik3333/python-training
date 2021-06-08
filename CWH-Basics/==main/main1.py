79f94487587a30ca89dca33175049c7a47b1604b
pratik kagale <pratikkagale3333@gmail.com>
2021-06-07 18:17:08 -0700

    if__name__==__main__ usage & necessity

---
+++ CWH-Basics/==main/main1.py
@@ -0,0 +1,13 @@
+
+
+def printhar(string):
+    return f"an unusual and exciting or daring exprience{string}."
+
+def add(num1,num2):
+    return num1+num2
+
+print("and the name is",__name__)
+if __name__ == '__main__':
+    o = add(4, 6)
+    print(o)
+
