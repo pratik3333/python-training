8df250df59ef9c0b911b91f1ee39c380882b8816
pratik kagale <pratikkagale3333@gmail.com>
2021-06-12 16:49:06 -0700

    generators (Iterables)

---
+++ CWH-Basics/Generators/A.py
@@ -0,0 +1,26 @@
+"""
+Iterables
+Iterator
+Iteration
+"""
+
+def gen(n):
+    for i in range (n):
+        yield i
+
+#print(gen(100000000))
+#
+# for i in gen(1000):
+#     print(i)
+#
+# obj1=gen(100)
+# print(next(obj1))
+# print(next(obj1))
+# print(next(obj1))
+
+
+obj1=gen(3)
+print(next(obj1))
+print(next(obj1))
+print(next(obj1))
+print(next(obj1))
