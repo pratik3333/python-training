fa1d1fccd8310a521af7bc21989b6ddbd5651282
pratik kagale <pratikkagale3333@gmail.com>
2021-06-12 11:53:50 -0700

    if __name__=='main__'

---
+++ CWH-Basics/Intermediate/__name__==__main/a.py
@@ -0,0 +1,13 @@
+import os
+def mostimpfunc():
+    print("Adventure is calling")
+
+def pratik():
+    print(os.listdir("/"))
+    print("Wilderness")
+if (__name__ == "__main__"):
+   pratik()
+
+
+mostimpfunc()
+#pratik()
---
+++ CWH-Basics/Intermediate/__name__==__main/b.py
@@ -0,0 +1,4 @@
+import a
+
+a.mostimpfunc()
+#a.pratik()
