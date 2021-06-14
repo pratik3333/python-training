5847f503dbeb9db8a44fa9a7749631d416a19a00
pratik kagale <pratikkagale3333@gmail.com>
2021-06-14 12:04:01 -0700

    request module

--- corey/REQUEST/C.py
+++ corey/REQUEST/C.py
@@ -1,10 +1,10 @@
 import requests
 
 
-#r=requests.get('https://httpbin.org/basic-auth/pratik/testing', auth=('pratik','testing'))
-#print(r) #<Response[200]> = success
+r=requests.get('https://httpbin.org/basic-auth/pratik/testing', auth=('pratik','testing'))
+print(r.json()) #<Response[200]> = success
 
 
 
-r=requests.get('https://httpbin.org/basic-auth/pratik/testing', auth=('pratikms','testing'))
-print(r) #<Response[401]> = unauthorised code
+#r=requests.get('https://httpbin.org/basic-auth/pratik/testing', auth=('pratikms','testing'))
+#print(r) #<Response[401]> = unauthorised code
