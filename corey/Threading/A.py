240c3793efbcd03c4132919af06a561d7d29d9e0
pratik kagale <pratikkagale3333@gmail.com>
2021-06-17 10:12:11 -0700

    threading.Thread

--- corey/Threading/A.py
+++ corey/Threading/A.py
@@ -4,19 +4,19 @@ import threading
 start=time.perf_counter()
 
 def do_something(seconds):
-    print(f'Sleeping {seconds}second(s)...')
+    print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
     print('Done sleeping...')
 
-threds=[]
+threads=[]
 
-for _ in range(100):
+for _ in range(10):
     t=threading.Thread(target=do_something,args=[1.5])
     t.start()
-    threds.append(t)
+    threads.append(t)
 
-for thred in threds:
-    thred.join()
+for thread in threads:
+    thread.join()
 
 
 
