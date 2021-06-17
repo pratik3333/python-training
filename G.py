729191f963550f938246b18dc0df363b025e5b0e
pratik kagale <pratikkagale3333@gmail.com>
2021-06-16 22:18:59 -0700

    itertools.product and itertools.combinations_with_replacement

--- G.py
+++ G.py
@@ -2,7 +2,8 @@ import itertools
 
 number=[0,1,2,3]
 
-result=itertools.product(number,repeat=4)
+#result=itertools.product(number,repeat=4)
+result=itertools.combinations_with_replacement(number,4)
 
 
 for item in result:
