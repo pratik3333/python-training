0e746c29eb964d7b39f8dc7328b70c4aee7d31a7
pratik kagale <pratikkagale3333@gmail.com>
2022-05-27 14:56:54 +0530

    dictioanary using for loop

--- clg/dicti.py
+++ clg/dicti.py
@@ -4,10 +4,10 @@ de={'one':'abc','two':'atc','three':'ytr','four':'cndj'}
 w=input("Enter a number for searching: ")
 
 
-while w==de:
-    de.values()
-    print(w)
-#
-# x=de.get()
-# for i in de.values(x):
-#     print(x)
+
+for i,j in de.items():
+    if i==w:
+        print(j)
+    else:
+        continue
+
