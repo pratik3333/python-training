543b059f2aadeb8bc14ae903edfa401f330881e3
pratik kagale <pratikkagale3333@gmail.com>
2022-05-27 14:39:55 +0530

    something went wrong

---
+++ clg/dicti.py
@@ -0,0 +1,13 @@
+
+de={'one':'abc','two':'atc','three':'ytr','four':'cndj'}
+
+w=input("Enter a number for searching: ")
+
+
+while w==de:
+    de.values()
+    print(w)
+#
+# x=de.get()
+# for i in de.values(x):
+#     print(x)
