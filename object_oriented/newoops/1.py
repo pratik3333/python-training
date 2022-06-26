3d934f4b8f5c0317a79d3edab6f781569267da68
pratik kagale <pratikkagale3333@gmail.com>
2022-06-27 00:50:10 +0530

    updated

--- object_oriented/newoops/1.py
+++ object_oriented/newoops/1.py
@@ -1,8 +1,8 @@
 class Item:
     def __init__(self,name: str,price: float,quantity=0):
         #Run validation to the received arguments
-        assert price >= 0
-        assert quantity >= 0
+        assert price >= 0, f"Price {price} is not greater than or equal to Zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
 
         #Assign to self object
         self.name = name
@@ -12,7 +12,7 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("Phone",100,1)
+item1 = Item("Phone",100,-1)
 
 item2=Item("Laptop",1000,3)
 
