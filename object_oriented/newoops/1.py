92765bbd32a7a768854593e13809f6a640bf0d59
pratik kagale <pratikkagale3333@gmail.com>
2022-06-27 22:48:21 +0530

    added discount

--- object_oriented/newoops/1.py
+++ object_oriented/newoops/1.py
@@ -1,8 +1,10 @@
 class Item:
+    pay_rate = 0.8 #The pay rate after 20% discount
     def __init__(self,name: str,price: float,quantity=0):
         #Run validation to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to Zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
 
         #Assign to self object
         self.name = name
@@ -12,7 +14,11 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
-item1 = Item("Phone",100,1)
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
+item1 = Item("Phone",100,1)
 item2=Item("Laptop",1000,3)
 
+item1.apply_discount()
+print(item1.price)
