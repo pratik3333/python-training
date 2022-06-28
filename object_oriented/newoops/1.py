5104b6f5cad3fa791e26f2dee9a934b38ad27788
pratik kagale <pratikkagale3333@gmail.com>
2022-06-28 23:05:38 +0530

    added __repr__ function

--- object_oriented/newoops/1.py
+++ object_oriented/newoops/1.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8 #The pay rate after 20% discount
+    all = []
     def __init__(self,name: str,price: float,quantity=0):
         #Run validation to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to Zero!"
@@ -11,18 +12,25 @@ class Item:
         self.price=price
         self.quantity=quantity
 
+        #Action to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
-item1 = Item("Phone",100,1)
+    def __repr__(self):
+        return f"Item('{self.name}, {self.price}, {self.quantity}')"
+
+item1=Item("Phone",100,1)
 item2=Item("Laptop",1000,3)
+item3=Item("Cable",10,5)
+item4=Item("Mouse",50,5)
+item5=Item("Keyboard",75,5)
 
-item1.apply_discount()
-print(item1.price)
+for instance in Item.all:
+    print(instance.name)
 
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+print(Item.all)
