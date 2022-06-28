cabf164b81b25495cb24abb26eab5984f8f9e0f7
pratik kagale <pratikkagale3333@gmail.com>
2022-06-28 22:45:21 +0530

    added another file for practice

--- object_oriented/newoops/1.py
+++ object_oriented/newoops/1.py
@@ -15,10 +15,14 @@ class Item:
         return self.price * self.quantity
 
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
 
 item1 = Item("Phone",100,1)
 item2=Item("Laptop",1000,3)
 
 item1.apply_discount()
 print(item1.price)
+
+item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
