53691795309abd6ace026a45aa3fd0e2d945f996
pratik kagale <pratikkagale3333@gmail.com>
2022-06-29 00:43:10 +0530

    read csv file from classmethod

--- object_oriented/newoops/1.py
+++ object_oriented/newoops/1.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     pay_rate = 0.8 #The pay rate after 20% discount
     all = []
@@ -21,16 +23,19 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('Item.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            print(item)
+
+
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity}')"
 
-item1=Item("Phone",100,1)
-item2=Item("Laptop",1000,3)
-item3=Item("Cable",10,5)
-item4=Item("Mouse",50,5)
-item5=Item("Keyboard",75,5)
-
-for instance in Item.all:
-    print(instance.name)
-
-print(Item.all)
+# for instance in Item.all:
+#     print(instance.name)
+Item.instantiate_from_csv()
