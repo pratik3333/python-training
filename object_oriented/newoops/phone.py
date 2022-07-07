54621f88f5a76ed463335c3e1b35a3c2a4d769e6
pratik kagale <pratikkagale3333@gmail.com>
2022-07-08 00:40:08 +0530

    Inheritance and polymorphism

--- object_oriented/newoops/phone.py
+++ object_oriented/newoops/phone.py
@@ -2,6 +2,7 @@
 from item import Item
 
 class Phone(Item):
+    pay_rate = 0.5
     def __init__(self,name: str,price: float,quantity=0, broken_phones=0):
        # Call to super function to have access to all attributes / methods
        super(Phone, self).__init__(
