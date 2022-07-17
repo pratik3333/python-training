0421d09a43bcd2884dd77b6af5829d484ac70baf
pratik kagale <pratikkagale3333@gmail.com>
2022-07-18 01:03:48 +0530

    Contact page Done

--- Django_CLG_Project/django_project/home/views.py
+++ Django_CLG_Project/django_project/home/views.py
@@ -1,11 +1,13 @@
+from cgitb import html
+import email
+from operator import concat
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
-    context = {
-        "variable":"This is sent"
-    }
-    return render(request, 'index.html',context)
+    return render(request, 'index.html')
     #return HttpResponse("This is home page")
 
 def about(request):
@@ -17,7 +19,14 @@ def services(request):
     #return HttpResponse("This is services page")
 
 def contact(request):
-    return render(request, 'contact.html', )
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, phone=phone, email=email, desc=desc, date=datetime.today())
+        contact.save()
+    return render(request,'contact.html', )
    # return HttpResponse("This is contact page")
 
 def Icecream(request):
